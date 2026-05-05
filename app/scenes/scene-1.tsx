85afbb396e491409723ff8e512d0329fa4b24d55
Jhasparr <88265780+Jhasparr@users.noreply.github.com>
2026-05-05 11:38:56 +0100

    mobile

--- app/scenes/scene-1.tsx
+++ app/scenes/scene-1.tsx
@@ -1,20 +1,28 @@
-'use client'
+"use client";
 
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { Robot_1 } from '../models/robot-1'
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Robot_1 } from "../models/robot-1";
+import { useEffect, useState } from "react";
 
 export default function Scene_1() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const check = () => setIsMobile(window.innerWidth < 768);
+    check();
+    window.addEventListener("resize", check);
+    return () => window.removeEventListener("resize", check);
+  }, []);
   return (
-    <div className='w-full h-full'>
-    <Canvas camera={{ position: [3, 2, 5], fov: 65 }}>
-      <ambientLight intensity={1} />
-      <directionalLight position={[5, 5, 5]} />
+    <div className="w-full h-full">
+      <Canvas camera={{ position: [3, 2, 5.5], fov: 65 }}>
+        <ambientLight intensity={1} />
+        <directionalLight position={[2, 4, 6]} />
 
-      <Robot_1 scale={0.6} />
-
-      <OrbitControls />
-    </Canvas>
+        <Robot_1 scale={0.6} position={[0, -1, 0]} />
+        {!isMobile && <OrbitControls enableZoom={false}/>}
+      </Canvas>
     </div>
-  )
+  );
 }
